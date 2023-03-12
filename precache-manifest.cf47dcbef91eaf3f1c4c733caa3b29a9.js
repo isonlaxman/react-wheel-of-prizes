c7e29bded2bcb8049573ddf33617b01de6158fe0
Laxman Soni <inos1199@gmail.com>
2023-03-12 08:57:08 -0400

    Updates

---
+++ precache-manifest.cf47dcbef91eaf3f1c4c733caa3b29a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f976c3f38c938b7ffc3c3e50d37569a7",
+    "url": "/react-wheel-of-prizes/index.html"
+  },
+  {
+    "revision": "07f57a62041c551435cc",
+    "url": "/react-wheel-of-prizes/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "5c153babe9a163a59ca7",
+    "url": "/react-wheel-of-prizes/static/js/2.e9af3afc.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react-wheel-of-prizes/static/js/2.e9af3afc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07f57a62041c551435cc",
+    "url": "/react-wheel-of-prizes/static/js/main.eff1db42.chunk.js"
+  },
+  {
+    "revision": "e97ed5b964ea89f93eeb",
+    "url": "/react-wheel-of-prizes/static/js/runtime-main.10dfc15b.js"
+  }
+]);
